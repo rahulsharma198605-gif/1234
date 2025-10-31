f2570be1b142d9790f70ed2bbda90cac52b1604b
rahulsharma198605-gif <rahulsharma198605@gmail.com>
2025-10-31 19:43:25 +0530

    Change in Message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello from the CI → Docker pipeline!"
+    return "Hello, This is the testing of complete automation till Git to Docker, If successfull "HURRAH"!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
